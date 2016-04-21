7c8fd3eacc551b3888951a227105af12ab7823e3
Khanthulhu <johnlrbrock@gmail.com>
2016-04-20 22:07:28 -0400

    Ease of reading
    Added comments to most functions explaining purpose. Moved key and user_id to top to make it easier for other users to use program.

--- Goodreads.py
+++ Goodreads.py
@@ -14,18 +14,21 @@ import re
 import requests
 import time
 
-start = time.time()
+# Type in your Goodreads developer key and User id
+key = ""
+user_id = ""
 
-
+# pull text from xml data and append to a list
 def get_text(tree, tg, lst):
     for element in tree.iter(tag=tg):
         lst.append(element.text)
-
+# use get_text() to extract book titles, authors, and page numbers 
 def get_data():
     get_text(tree, 'title', titles)
     get_text(tree, 'name', authors)
     get_text(tree, 'num_pages', pages)
 
+# take book title and author names and create the url that will be used to conduct the search
 def make_liburl(search_terms):
     liburl= "http://catalog.pascolibraries.org/cgi-bin/koha/opac-search.pl?idx=&q="
     for targets in re.findall('[(].+?[)]',search_terms):
@@ -35,11 +38,13 @@ def make_liburl(search_terms):
     liburl = liburl + "&limit=available&limit=itype:BOOK"
     return liburl
 
+# so we don't search for a book twice
 def remove_book(index):
     authors.remove(authors[index])
     titles.remove(titles[index])
     pages.remove(pages[index])
 
+# randomly choose a book and search for it in the library catalogue
 def search_book():
     global page_total
     global book_total
@@ -63,42 +68,46 @@ def search_book():
         remove_book(random_book)
     else:
         print("Couldn't find",titles[random_book],"by",authors[random_book])
-        webbrowser.open(search_url)
         book_total = book_total - 1
         remove_book(random_book)
 
-
+# lists for storing book data
 titles = []
 authors = []
 pages = []
+
 page_total = 0
-page_goal = int(input("How many pages do you want to read?\n"))
-key = input("Input your goodreads API key")
-secret = ""
 current_page = "1"
+page_goal = int(input("How many pages do you want to read?\n"))
+
+
+start = time.time()
 
 # get and parse data from goodreads
-url = "https://www.goodreads.com/review/list/12433997.xml?key="+key+"&shelf=to-read&per_page=200&page="+current_page
+url = "https://www.goodreads.com/review/list/"+user_id+".xml?key="+key+"&shelf=to-read&per_page=200&page="+current_page
 XML = requests.get(url)
 tree = ET.fromstring(XML.content)
 
-# how many pages do I need to search through?
+# find number of pages of xml data
 numpages = tree.find('./books').attrib['numpages']
 
 get_data()
 
 
-# cycle through the rest of the pages
+# cycle through any remaining pages of xml data
 while(int(numpages)>int(current_page)):
     current_page = str(int(current_page)+1)
-    url = "https://www.goodreads.com/review/list/12433997.xml?key="+key+"&shelf=to-read&per_page=200&page="+current_page
+    url = "https://www.goodreads.com/review/list/"+user_id+".xml?key="+key+"&shelf=to-read&per_page=200&page="+current_page
     XML = requests.get(url)
     tree = ET.fromstring(XML.content)
     get_data()
 
 book_total = len(titles)
 
+# search for random books until page goal is met
 while(page_goal>page_total):
     search_book()
+
+# how long did that take?
 end = time.time()
 print("Finished in",end - start,"seconds.\n")
